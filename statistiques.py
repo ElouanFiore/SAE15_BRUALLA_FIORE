996a7ec5f5106b1f9e17cd661464ca94b34d9d8e
Brualla Léo <leobrualla@gmail.com>
2022-01-25 11:06:04 +0100

    statistiques

---
+++ statistiques.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+
+def moyenne(L):
+	moy=0
+	for x in range(len(L)):
+		moy=L[x]+moy
+	moy=moy/len(L)
+	return moy
+
+def ecart_type(L):
+	moy=moyenne(L)
+	ecart=0
+	for x in range(len(L)):
+		ecart=ecart+(L[x]-moy)**2
+	ecart=sqrt(ecart/len(L))
+	return(ecart)
+
+def covar(x,y):
+	moyX=moyenne(x)
+	moyY=moyenne(y)
+	result=0
+	for index,val in enumerate(x):
+		result+=(val-moyX)*(y[index]-moyY)
+	return result/len(x)
