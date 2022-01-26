c8435c284a1232d0ba3442dd94349e3516b98e57
ElouanFiore <96867225+ElouanFiore@users.noreply.github.com>
2022-01-26 21:08:43 +0100

    prêt recup tout

--- main.py
+++ main.py
@@ -4,41 +4,67 @@ import statistiques as stat
 import time
 import os
 
+longueur = 10 #nombre de minute dans un jour
+periode = 2 #toutes les 5 minutes
+
+"""
+Used to make the dict of velo ids 
+re = requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")
+tree = etree.fromstring(re.content)
+li = list(tree)[0]
+res = dict()
+for i in li:
+	res[i.attrib["id"]] = i.attrib["na"][4:] + " vélo"
+
+print(res)
+"""
+
 url = "https://data.montpellier3m.fr/sites/default/files/ressources/{}.xml"
 
-endpoints_voiture = {'FR_MTP_CORU': 'Corum voiture'}
+endpoints_voiture = {'FR_MTP_ANTI': 'Antigone voiture', 'FR_MTP_COME': 'Comédie voiture', 'FR_MTP_CORU': 'Corum voiture', 'FR_MTP_EURO': 'Europa voiture', 'FR_MTP_FOCH': 'Foch voiture', 'FR_MTP_GAMB': 'Gambetta voiture', 'FR_MTP_GARE': 'Gare voiture', 'FR_MTP_TRIA': 'Triangle voiture', 'FR_MTP_ARCT': 'Arc de Triomphe voiture', 'FR_MTP_PITO': 'Pitot voiture', 'FR_MTP_CIRC': 'Circe voiture', 'FR_MTP_SABI': 'Sabines voiture', 'FR_MTP_GARC': 'Garcia Lorca voiture', 'FR_MTP_SABL': 'Sablassou voiture', 'FR_MTP_MOSS': 'Mosson voiture', 'FR_STJ_SJLC': 'Saint Jean le Sec voiture', 'FR_MTP_MEDC': 'Euromédecine voiture', 'FR_MTP_OCCI': 'Occitanie voiture', 'FR_CAS_VICA': 'Vicarello voiture', 'FR_MTP_GA109': 'Gaumont EST voiture', 'FR_MTP_GA250': 'Gaumont OUEST voiture', 'FR_CAS_CDGA': 'Charles de Gaulle voiture', 'FR_MTP_ARCE': 'Arceaux voiture', 'FR_MTP_POLY': 'Polygone voiture'}
 champs_voiture = {"Status": "Status", "Free": "Place(s) libre(s)", "Total": "Nombre de places"}
 
-endpoints_velo = {"TAM_MMM_VELOMAG": "velo"}
-id_velo = {"003": "Esplanade vélo", "005": "Corum vélo"}
-champs_velo = {"av": "Places occupées", "fr": "Places libres", "to": "Places totales"}
+endpoints_velo = ["TAM_MMM_VELOMAG"]
+id_velo = {'001': 'Rue Jules Ferry - Gare Saint-Roch vélo', '002': 'Comédie vélo', '003': 'Esplanade vélo', '004': 'Hôtel de Ville vélo', '005': 'Corum vélo', '006': 'Place Albert 1er - St Charles vélo', '007': 'Foch vélo', '008': 'Halles Castellane vélo', '009': 'Observatoire vélo', '010': 'Rondelet vélo', '011': 'Plan Cabanes vélo', '012': 'Boutonnet vélo', '013': 'Emile Combes vélo', '014': 'Beaux-Arts vélo', '015': 'Les Aubes vélo', '016': 'Antigone centre vélo', '017': 'Médiathèque Emile Zola vélo', '018': "Nombre d'Or vélo", '019': 'Louis Blanc vélo', '020': 'Gambetta vélo', '021': 'Port Marianne vélo', '022': 'Clemenceau vélo', '023': 'Les Arceaux vélo', '024': 'Cité Mion vélo', '025': 'Nouveau Saint-Roch vélo', '026': 'Renouvier vélo', '027': 'Odysseum vélo', '028': 'Saint-Denis vélo', '029': 'Richter vélo', '030': 'Charles Flahault vélo', '031': 'Voltaire vélo', '032': "Prés d'Arènes vélo", '033': 'Garcia Lorca vélo', '034': 'Vert Bois vélo', '035': 'Malbosc vélo', '036': 'Occitanie vélo', '037': 'FacdesSciences vélo', '038': 'Fac de Lettres vélo', '039': 'Aiguelongue vélo', '040': 'Jeu de Mail des Abbés vélo', '041': 'Euromédecine vélo', '042': 'Marie Caizergues vélo', '043': 'Sabines vélo', '044': 'Celleneuve vélo', '045': 'Jardin de la Lironde vélo', '046': 'Père Soulas vélo', '047': 'Place Viala vélo', '048': 'Hôtel du Département vélo', '049': 'Tonnelles vélo', '050': 'Parvis Jules Ferry - Gare Saint-Roch vélo', '051': 'Pont de Lattes - Gare Saint-Roch vélo', '053': 'Deux Ponts - Gare Saint-Roch vélo', '054': 'Providence - Ovalie vélo', '055': "Pérols Etang de l'Or vélo", '056': 'Albert 1er - Cathédrale vélo', '057': 'Saint-Guilhem - Courreau vélo', '059': 'Sud De France vélo'}
+champs_velo = {"av": "Vélos libres", "fr": "Places libres", "to": "Places totales"}
 
-voiture = API(url, log=2)
-velo = API(url, log=2)
+apivoiture = API(url, log=2)
+apivelo = API(url, log=2)
 
 def recup():
 	temps = time.strftime("%d/%m/%Y-%H:%M", time.localtime(time.time()))
 
-	voiture.downloadEndpoints(endpoints_voiture.keys())
-	voiture.processXML(champs_voiture.keys(), timecap=temps)
-	voiture.saveCSV(path="./databrutes")
+	apivoiture.downloadEndpoints(endpoints_voiture.keys())
+	apivoiture.processXML(champs_voiture.keys(), timecap=temps)
+	apivoiture.saveCSV(path="./databrutes")
 
-	velo.downloadEndpoints(endpoints_velo.keys())
-	velo.processXML(champs_velo.keys(), id=id_velo.keys(), timecap=temps)
-	velo.saveCSV(path="./databrutes")
+	apivelo.downloadEndpoints(endpoints_velo.keys())
+	apivelo.processXML(champs_velo.keys(), id=id_velo.keys(), timecap=temps)
+	apivelo.saveCSV(path="./databrutes")
 
-def compte_rendu(n, t, m, p, s):
+if input(f"Voulez vous lancer un récupération toutes les {periode} minutes pour {longueur} minutes [Y=oui] ? ").lower() == "y":
+	apivoiture.runFor(longueur, periode, recup)
+
+"""
+def compte_rendu_velo(n, t, m, p, s):
 	with open("compte_rendu.txt", "a") as f:
 		f.write(f"[{n}]\n")
-		f.write(f"	Nombres de vélos disponible maximum: {t}\n")
+		f.write(f"	Nombres de vélos disponibles maximum: {t}\n")
 		f.write(f"	Moyenne du nombres de vélos disponibles : {m}\n")
 		f.write(f"	Pourcentage moyen du nombres de vélos disponibles : {p}%\n")
-		f.write(f"	Ecart type à la moyenne : {s}\n")
+		f.write(f"	Écart type à la moyenne : {s}\n")
 		f.write("\n")
 		f.close()
 
-if input("Voulez-vous lancez une récupération ? [Y = oui ] ").lower() == "y":
-	recup()
+def compte_rendu_voiture(n, t, m, p, s):
+	with open("compte_rendu.txt", "a") as f:
+		f.write(f"[{n}]\n")
+		f.write(f"	Nombres de places disponibles maximum: {t}\n")
+		f.write(f"	Moyenne du nombres de places disponibles : {m}\n")
+		f.write(f"	Pourcentage moyen du nombres de places disponibles : {p}%\n")
+		f.write(f"	Écart type à la moyenne : {s}\n")
+		f.write("\n")
+		f.close()
 
 recupvelo  = CSV(id_velo.keys(), path="./databrutes")
 recupvoiture = CSV(endpoints_voiture.keys(), path="./databrutes")
@@ -86,3 +112,4 @@ with open("conf.plot", "w") as f:
 	f.close()
 
 os.system("gnuplot conf.plot")
+"""
--- recuperer.py
+++ recuperer.py
@@ -185,7 +185,7 @@ class CSV():
 if __name__ == "__main__":
 	url = "https://data.montpellier3m.fr/sites/default/files/ressources/{}.xml"
 	
-	endpointsvoi = {'FR_MTP_ANTI': 'Antigone', 'FR_MTP_COME': 'Comédie', 'FR_MTP_CORU': 'Corum', 'FR_MTP_EURO': 'Europa', 'FR_MTP_FOCH': 'Foch', 'FR_MTP_GAMB': 'Gambetta', 'FR_MTP_GARE': 'Gare', 'FR_MTP_TRIA': 'Triangle', 'FR_MTP_ARCT': 'Arc de Triomphe', 'FR_MTP_PITO': 'Pitot', 'FR_MTP_CIRC': 'Circe', 'FR_MTP_SABI': 'Sabines', 'FR_MTP_GARC': 'Garcia Lorca', 'FR_MTP_SABL': 'Sablassou', 'FR_MTP_MOSS': 'Mosson', 'FR_STJ_SJLC': 'Saint Jean le Sec', 'FR_MTP_MEDC': '    Euromédecine', 'FR_MTP_OCCI': 'Occitanie', 'FR_CAS_VICA': 'Vicarello', 'FR_MTP_GA109': 'Gaumont EST', 'FR_MTP_GA250': 'Gaumont OUEST', 'FR_CAS_CDGA': 'Charles de Gaulle', 'FR_MTP_ARCE': 'Arceaux', 'FR_MTP_POLY': 'Polygone'}
+	endpointsvoi = {'FR_MTP_ANTI': 'Antigone', 'FR_MTP_COME': 'Comédie', 'FR_MTP_CORU': 'Corum', 'FR_MTP_EURO': 'Europa', 'FR_MTP_FOCH': 'Foch', 'FR_MTP_GAMB': 'Gambetta', 'FR_MTP_GARE': 'Gare', 'FR_MTP_TRIA': 'Triangle', 'FR_MTP_ARCT': 'Arc de Triomphe', 'FR_MTP_PITO': 'Pitot', 'FR_MTP_CIRC': 'Circe', 'FR_MTP_SABI': 'Sabines', 'FR_MTP_GARC': 'Garcia Lorca', 'FR_MTP_SABL': 'Sablassou', 'FR_MTP_MOSS': 'Mosson', 'FR_STJ_SJLC': 'Saint Jean le Sec', 'FR_MTP_MEDC': 'Euromédecine', 'FR_MTP_OCCI': 'Occitanie', 'FR_CAS_VICA': 'Vicarello', 'FR_MTP_GA109': 'Gaumont EST', 'FR_MTP_GA250': 'Gaumont OUEST', 'FR_CAS_CDGA': 'Charles de Gaulle', 'FR_MTP_ARCE': 'Arceaux', 'FR_MTP_POLY': 'Polygone'}
 	champsvoi = {"DateTime": "Heure d'actualisation", "Name": "Nom du parking", "Status": "Status", "Free": "Place(s) libre(s)", "Total": "Nombre de places"}
 	
 	endpointsve = {"TAM_MMM_VELOMAG": "velo"}
